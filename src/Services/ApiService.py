4c04175c87f5fba056a50b582989c08fc91227fb
Javier Galego <jgalegop@gmail.com>
2023-12-29 11:07:25 +0100

    complete functionality in apiService

--- src/Services/ApiService.py
+++ src/Services/ApiService.py
@@ -1,15 +1,21 @@
 from sys import stderr
 import requests
+import csv
+import os
+from datetime import datetime
 
 
 class ApiService:
     def __init__(self):
         self.base_url = 'https://jsonplaceholder.typicode.com/todos/'
+        self.storage_folder = "storage"
+        os.makedirs(self.storage_folder, exist_ok=True)
 
     def run(self):
         print('Running ApiService', file=stderr)
 
         todos = self.fetch_data()
+        self.export_data(todos)
 
     def fetch_data(self):
         response = requests.get(self.base_url)
@@ -18,3 +24,14 @@ class ApiService:
         else:
             raise requests.exceptions.HTTPError("Failed to fetch data")
 
+    def export_data(self, data):
+        for d in data:
+            self.save_csv(d)
+            
+    def save_csv(self, data):
+        file_name = f"{datetime.now().strftime('%Y_%m_%d')}_{data['id']}.csv"
+        file_path = os.path.join(self.storage_folder, file_name)
+        with open(file_path, mode='w', newline='') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=data.keys())
+            writer.writeheader()
+            writer.writerow(data)
